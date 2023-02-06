8cc2545a2d6f9ca41804e9336ea338691c74294a
chrisgom113 <cgomez4@uci.edu>
2023-02-05 17:23:08 -0800

    added api fetch function, attached to search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,30 @@
 var APIKey = 'c29c595fed34bd03d71c486c172a3363';
 
 //Elements
-var searchBtnEL = $('#search-btn');
+var searchBtnEL = document.getElementById('search-btn')
+var cityInputEl = $('#city-input');
 
 
-
+searchBtnEL.addEventListener('click', fetchWeatherData);
 
 
 //Functions
-function fetchWeatherData () {
-console.log("did it work");
+function fetchWeatherData (cityRequested) {
+    var cityRequested = cityInputEl.val();
+    var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityRequested + '&appid=' + APIKey;
+
+    fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+    })
+
 }
 
 
 
 
-//Clicks
-searchBtnEL.on('click', fetchWeatherData);
+
 
