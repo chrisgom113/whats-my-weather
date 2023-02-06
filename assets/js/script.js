cd9768d5278fc424dd9dfb2f66c0fc23f64bf2e1
chrisgom113 <cgomez4@uci.edu>
2023-02-05 19:30:58 -0800

    added parse and dom traversal for temp, wind, and humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,41 @@
 var APIKey = 'c29c595fed34bd03d71c486c172a3363';
 
 //Elements
-var searchBtnEL = document.getElementById('search-btn')
+var searchBtnEl = document.getElementById('search-btn')
 var cityInputEl = $('#city-input');
+var cityDateDisplayEl = $('#city-date');
+var mainDisplayEl = $('#display-column');
+var tempDisplayEl = $('#temp');
+var windDisplayEl = $('#wind');
+var humidityDisplayEl = $('#humidity');
+
+//state vars
+currentDate = dayjs().format('M/D/YYYY')
 
 
-searchBtnEL.addEventListener('click', fetchWeatherData);
+searchBtnEl.addEventListener('click', fetchWeatherData);
 
 
 //Functions
-function fetchWeatherData (cityRequested) {
+
+//Fetch Weather Data
+function fetchWeatherData(cityRequested) {
     var cityRequested = cityInputEl.val();
-    var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityRequested + '&appid=' + APIKey;
+    var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityRequested + '&appid=' + APIKey + '&units=imperial';
 
     fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            mainDisplayEl.removeClass("d-none");
+            cityDateDisplayEl.text(data.name + ' (' + currentDate + ')');
+            tempDisplayEl.text('Temp: ' + data.main.temp + ' \u2109');
+            windDisplayEl.text('Wind: ' + data.wind.speed + ' MPH');
+            humidityDisplayEl.text('Humidity: ' + data.main.humidity + ' %');
 
+        })
 }
 
 
