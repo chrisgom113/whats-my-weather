08512be92b9ca5fb3afb1bd2225219863a610720
chrisgom113 <cgomez4@uci.edu>
2023-02-05 16:18:06 -0800

    added click even code to search button, set up some element refs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-//api key: c29c595fed34bd03d71c486c172a3363
+var APIKey = 'c29c595fed34bd03d71c486c172a3363';
+
+//Elements
+var searchBtnEL = $('#search-btn');
+
+
+
+
+
+//Functions
+function fetchWeatherData () {
+console.log("did it work");
+}
+
+
+
+
+//Clicks
+searchBtnEL.on('click', fetchWeatherData);
+
